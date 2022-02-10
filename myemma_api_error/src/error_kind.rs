96da91ba5b809bf03893b5626da98d37ac29ae1c
ralpha <ralph.bisschops.dev@gmail.com>
2022-02-10 16:53:39 +0100

    `ApiErrorKind` to and from `u16`

--- myemma_api_error/src/error_kind.rs
+++ myemma_api_error/src/error_kind.rs
@@ -29,3 +29,33 @@ impl ApiErrorKind {
         *self == ApiErrorKind::ServerError || *self == ApiErrorKind::PrivateError
     }
 }
+
+impl From<u16> for ApiErrorKind {
+    fn from(kind: u16) -> Self {
+        use ApiErrorKind::*;
+        match kind {
+            500 => ServerError,
+            400 => BadRequest,
+            401 => Unauthorized,
+            403 => Forbidden,
+            404 => NotFound,
+            422 => UnprocessableEntity,
+            _ => PrivateError,
+        }
+    }
+}
+
+impl From<ApiErrorKind> for u16 {
+    fn from(kind: ApiErrorKind) -> Self {
+        use ApiErrorKind::*;
+        match kind {
+            ServerError => 500,
+            BadRequest => 400,
+            Unauthorized => 401,
+            Forbidden => 403,
+            NotFound => 404,
+            UnprocessableEntity => 422,
+            PrivateError => 500,
+        }
+    }
+}
